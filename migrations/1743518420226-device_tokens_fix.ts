0f0fe17550d428a234d906ef1e334bd978a31dd9
Mark <marko.solomchak.work@gmail.com>
2025-06-03 16:17:18 +0300

    fix

--- migrations/1743518420226-device_tokens_fix.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
-
-export class DeviceTokensFix1743518420226 implements MigrationInterface {
-  name = 'DeviceTokensFix1743518420226';
-
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
