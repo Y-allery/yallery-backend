d3cbca38bded6bb875305fa037e137b8341e07e5
Mark <marko.solomchak.work@gmail.com>
2025-07-08 23:39:59 +0300

    fix

--- src/video-generation/video-generation.controller.ts
+++ src/video-generation/video-generation.controller.ts
@@ -17,7 +17,7 @@ export class VideoGenerationController {
   getAllAISettings() {
     return this.videoGenerationService.getAllAISettings();
   }
-  //sdasdasdівівsdsdsdів
+  //sdasdasdівівsdsdsdівsd
   @Post('generate-video')
   async generateOctoAI(
     @Body() createVideoDto: GenerateVideoDto,
