052b57441dca4d2ee8ab8ca8353fee2bb20eb54d
Mark <marko.solomchak.work@gmail.com>
2025-12-02 20:33:43 +0200

    Add migration to ensure video model exists in database

---
+++ migrations/1764700410883-ensure-video-model-exists.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class EnsureVideoModelExists1764700410883 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
